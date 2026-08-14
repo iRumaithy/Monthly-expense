9b802a0fcb70cafd235ed1850f7e6e01f27abea4
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-14 23:45:35 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='monthly-expense-v5-10-0-strict-consensus';
+const CACHE='monthly-expense-v6-1-0-adaptive-consensus';
 const CORE=['/','/index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('monthly-expense-v')&&k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
