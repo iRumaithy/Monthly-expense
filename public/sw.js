37fe8421fc3dab4cf394d5f1918afd59908f12df
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-17 09:01:14 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='monthly-expense-v6-0-0-manual-evidence';
+const CACHE='monthly-expense-v6-0-1-auto-totals-changelog';
 const SHELL_KEY='/__monthly_expense_app_shell__';
 const STATIC_CORE=['/manifest.webmanifest','/icon-192.png','/icon-512.png','/icon-maskable-512.png','/apple-touch-icon.png'];
 
